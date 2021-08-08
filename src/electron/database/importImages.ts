b86d3e6e0c9916e9b5a2eb109d5af127876b6e4f
Marshall Brain <marshalldbrain@gmail.com>
2021-08-07 20:55:19 -0400

    Implemented metadata filtering

--- src/electron/database/importImages.ts
+++ src/electron/database/importImages.ts
@@ -1,5 +1,30 @@
-import {ImageFile, Mapper} from "@components/dialogs/import_images/ImportImages";
+import {ImageFile, Mapper, Metadata, Transform} from "@components/dialogs/import_images/ImportImages";
+import fs from "fs";
+import dotProp from "dot-prop";
 
 export const importImages = (files: ImageFile[], mappers: Mapper[]) => {
-    console.log(files, mappers)
+    if (files.length == 0) return
+    for (const file of files) {
+        try {
+            const jsonData = JSON.parse(fs.readFileSync(file.path + ".json", 'utf8'))
+            let maps: Transform[] = []
+            for (const mapper of mappers) {
+                let matches = true
+                for (const filter of mapper.filters) {
+                    matches = matches && dotProp.get(jsonData, filter.path) === filter.value
+                }
+                if (matches) {
+                    maps = mapper.transforms
+                    break
+                }
+            }
+            const data: {[key in Metadata]?: string} = {}
+            for (const map of maps) {
+                data[map.metadata] = jsonData[map.prop]
+            }
+            console.log(data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 }
