6c55472c335abb1349b0ce6d6ad94135f6658d90
Marshall Brain <marshalldbrain@gmail.com>
2021-08-09 15:45:04 -0400

    Optimize importImages slightly

--- src/electron/database/importImages.ts
+++ src/electron/database/importImages.ts
@@ -1,30 +1,36 @@
-import {ImageFile, Mapper, Transform} from "@components/dialogs/import_images/ImportImages";
+import {ImageFile, Mapper} from "@components/dialogs/import_images/ImportImages";
 import fs from "fs";
 import dotProp from "dot-prop";
-import {insert} from "@electron/database/database";
+import {db} from "@electron/database/database";
+import {columns} from "@electron/database/dbStructure";
 
 export const importImages = (files: ImageFile[], mappers: Mapper[]) => {
+    const cols = Object.values(columns.images).slice(1)
+    const insert = db.prepare(
+        `insert into images (${cols.join(", ")}) values (${cols.map(() => "?").join(", ")})`
+    )
     if (files.length == 0) return
     for (const file of files) {
         try {
             const jsonData = JSON.parse(fs.readFileSync(file.path + ".json", 'utf8'))
-            let maps: Transform[] = []
+            let maps: {[key: string]: string} = {}
             for (const mapper of mappers) {
                 let matches = true
                 for (const filter of mapper.filters) {
                     matches = matches && dotProp.get(jsonData, filter.path) === filter.value
                 }
                 if (matches) {
-                    maps = mapper.transforms
+                    maps = Object.fromEntries(mapper.transforms.map(value => [value.metadata.toLowerCase(), value.prop]))
                     break
                 }
             }
-            const data: {[key: string]: string} = {}
-            for (const map of maps) {
-                data[map.metadata] = jsonData[map.prop]
-            }
-            data.original_metadata = JSON.stringify(jsonData)
-            insert("images", data)
+
+            const data: string[] = cols.map(value => {
+                if (value === "original_metadata") return JSON.stringify(jsonData)
+                return jsonData[maps[value]] || "null"
+            })
+            const imageId = insert.run(data).lastInsertRowid
+            console.log(imageId)
         } catch (e) {
             console.log(e.message)
         }
