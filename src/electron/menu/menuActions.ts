c8d9fffa60bf6dc3d78a75a2e1cf1852a8b0cb39
Marshall Brain <marshalldbrain@gmail.com>
2021-08-09 02:57:53 -0400

    Fix import

--- src/electron/menu/menuActions.ts
+++ src/electron/menu/menuActions.ts
@@ -1,4 +1,5 @@
 import {BrowserWindow, MenuItem} from "electron";
+import {openImportDialogChannel} from "@electron/ipcCommands";
 
 export const handleMenuItemClick = (menuItem: MenuItem, browserWindow: BrowserWindow, _event: any) => {
     switch (menuItem.label) {
