fb00a0e257837283717b4540132f3b97dbf0846d
marshallbrain <marshalldbrain@gmail.com>
2022-01-29 17:15:59 -0500

    Partially implement tagging

--- src/react/image_viewer/components/MetadataEdit.tsx
+++ src/react/image_viewer/components/MetadataEdit.tsx
@@ -1,17 +1,50 @@
-import {Drawer, styled, TextField} from '@mui/material';
-import React, {useEffect} from 'react';
+import {Chip, Divider, Drawer, ListItem, Paper, Stack, styled, TextField} from '@mui/material';
+import React, {KeyboardEvent, useEffect} from 'react';
 import {ImageData} from "./AppViewer";
 import {channels} from "@utils/ipcCommands";
+import sqlQueries from "@utils/sqlQueries";
+import {FixedSizeList, ListChildComponentProps} from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 const MetadataEdit = (props: PropTypes) => {
 
     const {editOpen, drawerWidth, imageData} = props
 
     const [title, setTitle] = React.useState(imageData? imageData?.title : "Undefined");
+    const [imageTags, setImageTags] = React.useState<Set<string>>(new Set())
+    const [tagsOrdered, setTagsOrdered] = React.useState<string[]>([])
+    const [tags, setTags] = React.useState<{ [index: string]: {} }>({})
+    const [tagSearch, setTagSearch] = React.useState("")
+
+    useEffect(() => {
+        window.api.db.getImages(sqlQueries.getTags, ([data]) => {
+            console.log(data)
+        }, {name: tagSearch})
+    }, [])
 
     useEffect(() => {
         setTitle(imageData? imageData?.title : "Undefined")
     }, [imageData])
+
+    const onSearchTags = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTagSearch(event.target.value);
+    }
+
+    const selectTag = (tag: string) => () => {
+        console.log(tag)
+    }
+
+    const renderTags = ({ index, style }: ListChildComponentProps) => (
+        <ListItem style={style} key={index} component="div" disablePadding>
+            {(index == 0 && tagSearch && tagsOrdered[index] !== tagSearch) ?
+                <Chip label={tagSearch} color={"success"} onClick={selectTag(tagSearch)}/>:
+                <Chip
+                    label={tagsOrdered[Math.max(index-1,0)]}
+                    onClick={selectTag(tagsOrdered[Math.max(index-1,0)])}
+                />
+            }
+        </ListItem>
+    )
 
     return (
         <Drawer
@@ -26,19 +59,57 @@ const MetadataEdit = (props: PropTypes) => {
             anchor="right"
             open={editOpen}
         >
-            <Pad>
-                <TextField label="Title" variant="outlined" value={title}/>
-            </Pad>
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="stretch"
+                spacing={2}
+                sx={{
+                    padding: 2
+                }}
+            >
+                <Paper
+                    sx={{
+                        p: 1,
+                        pb: 0
+                    }}
+                >
+                    <TextField
+                        label="Search Tags"
+                        variant="standard"
+                        value={tagSearch}
+                        onChange={onSearchTags}
+                        onKeyDown={(e: KeyboardEvent) => {
+                            if (e.key === "Enter") {
+                                e.preventDefault()
+                                selectTag(tagSearch)()
+                            }
+                        }}
+                    />
+                    <div
+                        style={{
+                            minHeight: 300
+                        }}
+                    >
+                        <AutoSizer>
+                            {({height, width}) => (
+                                <FixedSizeList
+                                    height={height}
+                                    width={width}
+                                    itemSize={42}
+                                    itemCount={+(tagSearch !== "") + tagsOrdered.length}
+                                    overscanCount={5}
+                                >
+                                    {renderTags}
+                                </FixedSizeList>
+                            )}
+                        </AutoSizer>
+                    </div>
+                </Paper>
+            </Stack>
         </Drawer>
     )
 }
-
-const Pad = styled("div")({
-    padding: 16,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "stretch",
-})
 
 interface PropTypes {
     editOpen: boolean,
