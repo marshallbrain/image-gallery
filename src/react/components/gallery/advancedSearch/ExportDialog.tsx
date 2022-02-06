5b833f9d38d2a37027c4458426060f02d3ace456
marshallbrain <marshalldbrain@gmail.com>
2022-02-06 09:59:41 -0500

    Populate export dialog

--- src/react/components/gallery/advancedSearch/ExportDialog.tsx
+++ src/react/components/gallery/advancedSearch/ExportDialog.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
-import {Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from "@mui/material";
+import React, {useState} from 'react/index';
+import {
+    Autocomplete,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle, FormControl, InputLabel, MenuItem,
+    Select, SelectChangeEvent, Stack, styled,
+    TextField, Typography
+} from "@mui/material";
+import {ImageFile} from "@components/dialogs/import_images/ImportImages";
 
 const ExportDialog = (props: PropTypes) => {
 
@@ -9,15 +20,54 @@ const ExportDialog = (props: PropTypes) => {
         selected
     } = props
 
+    const [title, setTitle] = useState("image_id")
+    const [folder, setFolder] = useState("")
+
+    const changeTitle = (event: SelectChangeEvent) => {
+        setTitle(event.target.value)
+    }
+
+    const getExportFolder = () => {
+
+    }
+
     return (
         <Dialog
             open={open}
-            maxWidth={"sm"}
-            fullWidth
             onClose={toggle}
+            fullWidth
+            maxWidth={"xs"}
         >
             <DialogTitle>Export Images</DialogTitle>
             <DialogContent>
+                <Stack
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="stretch"
+                    spacing={2}
+                    sx={{pt: 2}}
+                >
+                    <FormControl variant="standard">
+                        <InputLabel>Use as title</InputLabel>
+                        <Select
+                            value={title}
+                            onChange={changeTitle}
+                        >
+                            <MenuItem value={"image_id"}>Image Id</MenuItem>
+                            <MenuItem value={"title"}>Title</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Button
+                        variant="contained"
+                        sx={{alignSelf: "flex-start"}}
+                        onClick={getExportFolder}
+                    >
+                        select folder
+                    </Button>
+                    {folder !== "" && <Typography>
+                        {folder}
+                    </Typography>}
+                </Stack>
             </DialogContent>
             <DialogActions>
                 <Button onClick={toggle} >Cancel</Button>
@@ -26,6 +76,12 @@ const ExportDialog = (props: PropTypes) => {
         </Dialog>
     )
 }
+
+const Input = styled('input')({
+    display: 'none',
+    imageText: {},
+    alignSelf: "flex-start"
+});
 
 interface PropTypes {
     open: boolean
