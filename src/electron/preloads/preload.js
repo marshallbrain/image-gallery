49a0a31128f51ef30f8bcbf0fa245dd717f322d7
marshallbrain <marshalldbrain@gmail.com>
2022-02-06 13:26:50 -0500

    Implement once and request commands

--- src/electron/preloads/preload.js
+++ src/electron/preloads/preload.js
@@ -6,6 +6,7 @@ const {savedStorePreload} = require("./preloadSavedStore");
 const {systemPreload} = require("./preloadSystem");
 const {databasePreload} = require("./preloadDatabase");
 const {ipcChannels, logChannel} = require("../../utils/ipcCommands");
+const { v4: uuidv4 } = require('uuid');
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
@@ -13,6 +14,17 @@ contextBridge.exposeInMainWorld("api", {
     savedStore: savedStorePreload(ipcRenderer),
     system: systemPreload(ipcRenderer),
     db: databasePreload(ipcRenderer),
+    request: (channel, callback, ...data) => {
+        if (ipcChannels.has(channel)) {
+            const callBackChannel = uuidv4()
+            ipcRenderer.once(callBackChannel, (event, ...data) => {
+                callback(...data)
+            })
+            ipcRenderer.send(channel, {callBackChannel, data})
+        } else {
+            console.log(`Invalid send channel: "${channel}"`);
+        }
+    },
     send: (channel, ...data) => {
         if (ipcChannels.has(channel)) {
             ipcRenderer.send(channel, data);
@@ -24,6 +36,13 @@ contextBridge.exposeInMainWorld("api", {
         if (ipcChannels.has(channel)) {
             ipcRenderer.on(channel, (event, ...args) => func(...args));
             return func
+        } else {
+            console.log(logChannel, `Invalid receive channel: "${channel}"`);
+        }
+    },
+    once: (channel, func) => {
+        if (ipcChannels.has(channel)) {
+            ipcRenderer.once(channel, (event, ...args) => func(...args));
         } else {
             console.log(logChannel, `Invalid receive channel: "${channel}"`);
         }
