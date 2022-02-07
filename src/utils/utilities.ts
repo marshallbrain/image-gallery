b0756c776d0ad629c06ec8e8e1e8f1f9b8c175a8
marshallbrain <marshalldbrain@gmail.com>
2022-02-06 19:47:17 -0500

    Change where the resources folder is made

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -8,8 +8,10 @@ const exception = ():string => {
 
 export const isDev = process.env.NODE_ENV === "development";
 
+const above = (isDev)? "../": "../../../"
+
 export function appData(...path: string[]): string {
-    const fullPath = pathModule.join(app.getAppPath(), "../", "resources", ...path)
+    const fullPath = pathModule.join(app.getAppPath(), above, "resources", ...path)
     const parsedPath = pathModule.parse(fullPath)
 
     if (parsedPath.ext != "" && !fs.existsSync(parsedPath.dir)) {
