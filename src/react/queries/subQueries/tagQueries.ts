2a472006f2d702bbf299beef2885fe424e381626
marshallbrain <marshalldbrain@gmail.com>
2022-02-09 17:23:09 -0500

    Add other tag queries over

--- src/react/queries/subQueries/tagQueries.ts
+++ src/react/queries/subQueries/tagQueries.ts
@@ -4,5 +4,38 @@ export const tagGetQueries = {
             "select tag_id, name\n" +
             "from tags",
         order: "order by name"
+    },
+    getImageTags: {
+        query: "" +
+            "select t.tag_id, t.name\n" +
+            "from images_tags i\n" +
+            "left join tags t on i.tag_id = t.tag_id\n" +
+            "where i.image_id = ?\s",
+        order: "order by name"
+    },
+    createTag: {
+        query: "" +
+            "insert into tags (name)\n" +
+            "values(?)",
+        order: ""
+    },
+    addImageTag: {
+        query: "" +
+            "insert into images_tags\n" +
+            "select ?, ?",
+        order: ""
+    },
+    removeImageTag: {
+        query: "" +
+            "delete from images_tags\n" +
+            "where image_id = ? and tag_id = ?",
+        order: ""
+    },
+    clearImageTag: {
+        query: "" +
+            "delete from images_tags " +
+            "where image_id = ?",
+        order: ""
     }
+
 }
