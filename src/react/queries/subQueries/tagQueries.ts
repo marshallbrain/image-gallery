a862f1916aba4f32c6cfac01aa829c0ae383c73e
marshallbrain <marshalldbrain@gmail.com>
2022-02-09 18:25:26 -0500

    Update getTags query to search for matches first

--- src/react/queries/subQueries/tagQueries.ts
+++ src/react/queries/subQueries/tagQueries.ts
@@ -2,15 +2,16 @@ export const tagGetQueries = {
     getTags: {
         query: "" +
             "select tag_id, name\n" +
-            "from tags",
-        order: "order by name"
+            "from tags\n" +
+            "where name like '%' || @search || '%'",
+        order: "order by name = @search desc, name like @search || '%' desc, name"
     },
     getImageTags: {
         query: "" +
             "select t.tag_id, t.name\n" +
             "from images_tags i\n" +
             "left join tags t on i.tag_id = t.tag_id\n" +
-            "where i.image_id = ?\s",
+            "where i.image_id = ?\n",
         order: "order by name"
     },
     createTag: {
