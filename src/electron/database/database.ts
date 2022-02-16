2b49eb9d6714b0669d7f62193f2e198439e9964b
marshallbrain <marshalldbrain@gmail.com>
2022-02-16 17:38:07 -0500

    Removed comments

--- src/electron/database/database.ts
+++ src/electron/database/database.ts
@@ -43,38 +43,3 @@ const createChannelListeners = (
 
     searchQuery()
 }
-
-/*
-
-Image Database
---------------
-ID
-Title
-Author
-Group
-Category
-Collections
-Tags
-Rating
-Content Rating
-Source Website
-Date Added
-Date Modified
-Date Image Modified
-Size
-Width
-Height
-Description
-Other (Source Url)
-
-Author
-
-Group
-
-Category
-
-Collections
-
-Tags
-
- */
