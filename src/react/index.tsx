3f5960ae32e9a4d4c3e309172d7ba6e2717d0a09
Marshall Brain <marshalldbrain@gmail.com>
2022-06-29 23:18:45 -0400

    Update react entry point

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from "@react/Root";
+import './index.css';
+import {Button} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 ReactDOM.render(
     <Root/>,
     document.getElementById("root")
 );
+
+function Root() {
+
+    return (
+        <div>
+            <Button variant="contained" endIcon={<SendIcon />}>
+                Send
+            </Button>
+        </div>
+    );
+}
